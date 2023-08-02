c04348d3cc75c524790e774b16edb56561b1a65b
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-08-02 14:58:26 +0100

    created : the repository file that connects with the db, the service liar that connects with the repository, and the user.go file that connects them all

---
+++ server/internel/user/user.go
@@ -0,0 +1,29 @@
+package user
+
+import "context"
+
+type User struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRes struct {
+	ID       int64  `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+}
+
+type Service interface {
+}
