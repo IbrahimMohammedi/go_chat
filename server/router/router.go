aa9b150e9533d8669b7de1cd711e0304e6a25036
Ibrahim Mohammedi <ibrahimod77@gmail.com>
2023-08-07 13:08:13 +0100

    initiated the createhub in the main.go and router

--- server/router/router.go
+++ server/router/router.go
@@ -2,18 +2,20 @@ package router
 
 import (
 	"server/internel/user"
+	"server/internel/ws"
 
 	"github.com/gin-gonic/gin"
 )
 
 var r *gin.Engine
 
-func InitRouter(userHandler *user.Handler) {
+func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 	r = gin.Default()
 
 	r.POST("/signup", userHandler.CreateUser)
 	r.POST("/login", userHandler.Login)
 	r.GET("/logout", userHandler.Logout)
+	r.POST("/ws/createroom", wsHandler.CreateRoom)
 }
 
 func Start(addr string) error {
